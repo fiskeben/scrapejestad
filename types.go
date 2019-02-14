84cda5b95dced66f248511c494ac05167a1e6610
Ricco Førgaard <ricco@fiskeben.dk>
2019-02-14 20:01:48 +0100

    Changed JSON format
    `timestamp` is now a Unix timestamp
    and a new RFC3339 `date` field is added.

--- types.go
+++ types.go
@@ -8,14 +8,15 @@ import (
 
 // Reading represents one unique data point.
 type Reading struct {
-	SensorID string    `json:sensor_id`
-	Time     time.Time `json:"timestamp"`
+	SensorID string    `json:"sensor_id"`
+	Time     int64     `json:"timestamp"`
+	Date     time.Time `json:"date"`
 	Temp     float32   `json:"temperature"`
 	Humidity float32   `json:"humidity"`
 	Light    float32   `json:"light"`
 	PM25     float32   `json:"pm25"`
 	PM10     float32   `json:"pm10"`
-	Voltage  float32   `json:voltage"`
+	Voltage  float32   `json:"voltage"`
 	Firmware string    `json:"firmware_version"`
 	Position Position  `json:"coordinates"`
 	Fcnt     int       `json:"fcnt"`
@@ -34,7 +35,7 @@ PM10=%f
 Voltage=%f
 Firmware=%s
 Position=%s
-Fcnt=%d`, r.SensorID, r.Time.Format(time.RFC3339), r.Temp, r.Humidity, r.Light, r.PM25, r.PM10, r.Voltage, r.Firmware, r.Position.String(), r.Fcnt)
+Fcnt=%d`, r.SensorID, r.Date.Format(time.RFC3339), r.Temp, r.Humidity, r.Light, r.PM25, r.PM10, r.Voltage, r.Firmware, r.Position.String(), r.Fcnt)
 	gateways := make([]string, len(r.Gateways))
 	for i, g := range r.Gateways {
 		gateways[i] = fmt.Sprintf("  %d %s\n", i, g.String())
